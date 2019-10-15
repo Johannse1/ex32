0ff86771dae54ead15cffcb65df65226d0d91702
Evan <johannse@hartwick.edu>
2019-10-14 22:05:41 -0400

    update

---
+++ ex32A.py
@@ -0,0 +1,26 @@
+# evan johanns
+# ex 32
+# 10/14/19
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+for number in the_count:
+    print(f"this is count {number}")
+
+
+for fruit in fruits:
+    print(f" a type of fruit: {fruit}")
+
+for i in change:
+    print(f"i got {i}")
+
+elements = []
+for i in range(0,6):
+    print(f"adding {i} to the list.")
+    elements.append
+
+for i in elements:
+    print(f"element was: ")
+
